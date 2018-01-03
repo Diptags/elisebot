883fd5a4723c4307b0a8c600e5b016d17b6dd58e
Diptags <rd.pradipta@gmail.com>
2018-01-03 15:03:19 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Elisé (Chat Bot) #
+
+Masih diperbaharui, tunggu saja ya
+
+Data admin:
+
+* Repository    : https://github.com/Diptags
+* Email         : rd.pradipta@ui.ac.id
+* LINE ID       : Diptags
---
+++ data_longmsg.py
@@ -0,0 +1,90 @@
+# ---------------------- Long Message Center ---------------------- #
+
+help_msg = '''Halo kak, ini ada beberapa keyword khusus yang bisa dipakai:
+
+/help
+Menampilkan pesan bantuan ini
+
+/myprofile
+Menampilkan profil kakak (Hanya dalam chat 1:1)
+
+/sleep
+Menyuruh Elisé beristirahat selama 5 detik
+
+/quotes
+Menampilkan pesan dan kata mutiara yang berguna
+
+/admin
+Menampilkan informasi dari pembuat Elisé
+
+/translate
+Menerjemahkan bahasa EN -> ID atau ID -> EN
+
+/leave
+Menyuruh Elisé untuk meninggalkan obrolan
+
+Selain itu, aku juga bisa ngobrol lho
+Tapi kosakataku masih terbatas kak
+'''
+
+calc_msg = '''Elisé juga bisa berhitung lho~
+Caranya tinggal ketik:
+
+hitung <spasi> perhitungan
+
+Contoh:
+"hitung 4 + 7 * 4 - 2 / 2"
+'''
+
+kerang_msg = '''
+Pernahkah mencoba bot Kerang Ajaib yang melegenda itu?
+Atau pernah tahu namanya dari kartun S*pon*ge*bo*b ?
+
+Sekarang Elisé bisa juga lho~
+Cukup kasih aku pertanyaan dengan awalan
+"apakah" / "apa"
+
+Contoh:
+apakah admin ganteng
+apakah lord gaben baik'''
+
+kerang_jawab = ['Ya','Tidak','Bisa Jadi','Coba Tanya Lagi']
+
+trans_msg = """
+Halo kak ~ aku sekarang bisa bahasa asing lho.
+Untuk bertanya, cukup ketik:
+
+terjemahkan <spasi> bahasa tujuan <spasi> Kalimat yang mau diterjemahkan
+
+Aku baru bisa bahasa ini aja:
+ID (indonesia)
+EN (inggris)
+
+Contoh:
+terjemahkan EN saya lapar
+terjemahkan ID boring day
+
+Kosakataku masih terbatas
+jadi tolong jangan tanya yang lain ya~
+"""
+
+about_msg = '''    
+E-mail  : rd.pradipta@ui.ac.id
+Line ID : Diptags
+Lokasi  : Depok / Bogor, Indonesia
+Bagian dari TARUNG 2017 (Fasilkom UI 2017)
+
+Jika ingin membantu mengembangkan bot ini, mohon membaca 
+file CONTRIBUTING.md pada source code.
+'''
+
+admin_note_msg = ''' Halo, para pengguna Elisé (Chat bot)
+Terima kasih banyak telah menggunakan Elisé.
+
+Sampai saat ini, Elisé masihlah jauh dari kata sempurna, masih banyak hal yang harus dibenahi.
+Oleh karena itu, admin berpesan kepada para pengguna, jika menemukan bug atau memiliki kritik / saran, segeralah beritahu admin.
+Admin sangatlah terbuka menerima masukan dari para pengguna, terlebih jika Anda ingin berkontribusi, admin akan sangat senang
+
+Semoga bot sederhana ini bisa bermanfaat~
+Terima Kasih
+- Admin -'''
